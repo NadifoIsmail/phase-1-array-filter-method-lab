7747332354aab7bb906951d638096c702dab6525
Nadifo Ismail <nadifoismail80@gmail.com>
2024-10-08 14:04:31 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+const drivers = ["Mary","Anne","Antony"];
+const driversInfo = [
+    {
+        name: "Mary",
+        residence: "Nairobi",
+    },
+    {
+        name : "Anne",
+        residence : "Mombasa"
+    },
+    {
+        name : "Antony",
+        residence : "Nairobi",
+    },
+];
+function findMatching(drivers,name){
+return drivers.filter((element) => element.toLowerCase() === name.toLowerCase());
+
+}
+// console.log(findMatching(drivers,"ANNE"));
+function fuzzyMatch(drivers,name){
+return drivers.filter((element) => element.startsWith(name))
+}
+// console.log(fuzzyMatch(drivers,"A"));
+
+function matchName(drivers,name){
+    return drivers.filter((element) => element.name === name);
+}
+// console.log(matchName(driversInfo,"Antony"));
